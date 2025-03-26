31adeb2e5f619c45a706df4b9a061134f3ffcd6a
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-03-26 15:48:41 +0100

    Refactoring - fixed dashboard folder, made design changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      Hello World!
-      <Link href={"/settings"}>Go to settings</Link>
-      <Link href={"/dashboard"}>Go to dashboard</Link>
+    <div>
+      <h1>Hello World!</h1>
+      <div className={styles["links-container"]}>
+        <Link href={"/settings"}>Go to settings</Link>
+        <Link href={"/dashboard"}>Go to dashboard</Link>
+      </div>
     </div>
   );
 }
