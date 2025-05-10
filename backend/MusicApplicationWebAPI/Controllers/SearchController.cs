335e0d4596226e22071fa8b837536f35ebe51a1a
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-05-10 23:38:16 +0200

    Altered search so that tracks and albums of an artist are also shown when searching their name

--- backend/MusicApplicationWebAPI/Controllers/SearchController.cs
+++ backend/MusicApplicationWebAPI/Controllers/SearchController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MusicApplicationWebAPI.Data;
+using MusicApplicationWebAPI.Dtos;
+using MusicApplicationWebAPI.Dtos.MusicArtist;
+using MusicApplicationWebAPI.Dtos.MusicTrack;
 
 namespace MusicApplicationWebAPI.Controllers;
 [ApiController]
@@ -13,6 +16,7 @@ public class SearchController : Controller
     {
         _context = context;
     }
+
     [HttpGet]
     public async Task<IActionResult> Search([FromQuery] string term)
     {
@@ -22,8 +26,29 @@ public class SearchController : Controller
         var lowerTerm = term.ToLower();
 
         var albums = await _context.MusicAlbum
-            .Where(a => a.Title.ToLower().Contains(lowerTerm))
-            .Select(a => new { Type = "Music Album", MusicAlbum = a })
+            .Include(a => a.MusicArtistAlbums)
+                .ThenInclude(aa => aa.MusicArtist)
+            .Where(a =>
+                a.Title.ToLower().Contains(lowerTerm) ||
+                a.MusicArtistAlbums.Any(aa => aa.MusicArtist.Name.ToLower().Contains(lowerTerm))
+            )
+            .Select(a => new
+            {
+                Type = "Music Album",
+                MusicAlbum = new MusicAlbumDto
+                {
+                    Id = a.Id,
+                    Title = a.Title,
+                    CoverURL = a.CoverURL,
+                    UploadedAt = a.UploadedAt,
+                    ReleaseDate = a.ReleaseDate,
+                    MusicArtists = a.MusicArtistAlbums.Select(aa => new MusicArtistShortFormDto
+                    {
+                        Id = aa.MusicArtist.Id,
+                        Name = aa.MusicArtist.Name
+                    }).ToList()
+                }
+            })
             .ToListAsync();
 
         var artists = await _context.MusicArtist
@@ -32,9 +57,33 @@ public class SearchController : Controller
             .ToListAsync();
 
         var tracks = await _context.MusicTrack
-            .Where(t => t.Title.ToLower().Contains(lowerTerm))
-            .Select(t => new { Type = "Music Track", MusicTrack = t })
-            .ToListAsync();
+     .Include(t => t.MusicArtistTrack)
+         .ThenInclude(at => at.MusicArtist)
+     .Where(t =>
+         t.Title.ToLower().Contains(lowerTerm) ||
+         t.MusicArtistTrack.Any(at => at.MusicArtist.Name.ToLower().Contains(lowerTerm))
+     )
+     .Select(t => new
+     {
+         Type = "Music Track",
+         MusicTrack = new MusicTrackDto
+         {
+             Id = t.Id,
+             Title = t.Title,
+             ReleaseDate = t.ReleaseDate,
+             FilePath = t.FilePath,
+             IsExplicit = t.IsExplicit,
+             UploadedAt = t.UploadedAt,
+             CoverURL = t.CoverURL,
+             Duration = t.Duration,
+             MusicArtists = t.MusicArtistTrack.Select(at => new MusicArtistShortFormDto
+             {
+                 Id = at.MusicArtist.Id,
+                 Name = at.MusicArtist.Name
+             }).ToList()
+         }
+     })
+     .ToListAsync();
 
         var results = albums.Concat<object>(artists).Concat(tracks);
 
