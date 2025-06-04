35962e9f0caea06bd0607184c450b5b61642b7b5
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-06-04 23:53:27 +0200

    Started working on a page for music tracks

---
+++ frontend/src/app/search/track/[musicTrackId]/page.tsx
@@ -0,0 +1,76 @@
+import { MusicTrack } from '@/types/MusicTrack';
+import React from 'react'
+import styles from "./music-track-page.module.css"
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { getDurationInSeconds } from '@/utils/getDurationInSeconds';
+
+type Params = Promise<{ musicTrackId: string }>
+
+async function getMusicTrackById(musicTrackId: string): Promise<MusicTrack> {
+    try {
+        const res = await fetch(`${process.env.WEB_API_URL}/music-track/${musicTrackId}`);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch music track');
+        }
+
+        const musicTrack: MusicTrack = await res.json();
+        return musicTrack;
+    } catch (error) {
+        console.error('Error fetching music tracl:', error);
+        throw error;
+    }
+}
+
+function getTotalMinutes(musicTrack: MusicTrack): number {
+    const totalSeconds = getDurationInSeconds(musicTrack.duration);
+    return Math.floor(totalSeconds / 60);
+}
+
+export default async function MusicTrackPage({ params }: { params: Params }) {
+    const { musicTrackId } = await params;
+    const musicTrack = await getMusicTrackById(musicTrackId);
+    const musicTrackDate: Date = new Date(musicTrack?.release_date);
+    const musicTrackDateFormatted: string = new Intl.DateTimeFormat(navigator.language, {
+        year: 'numeric', month: 'short', day: 'numeric'
+    }).format(musicTrackDate);
+    const musicTrackLength = getTotalMinutes(musicTrack);
+
+    return (
+        <div className={styles["main-container"]}>
+            <h1>{musicTrack.title}</h1>
+            <div className={styles["info-containers-wrapper"]}>
+                <div className={styles["info-container"]}>
+                    <div className={styles["info-container-title-icon-box"]}>
+                        <div className={styles["info-container-icon"]}><CalendarMonthIcon /></div>
+                        <div className={styles["info-container-title"]}>Release</div>
+                    </div>
+                    <div className={styles["info-container-value"]}>
+                        {musicTrackDateFormatted}
+                    </div>
+                </div>
+                <div className={styles["info-container"]}>
+                    <div className={styles["info-container-title-icon-box"]}>
+                        <div className={styles["info-container-icon"]}><AccessTimeIcon /></div>
+                        <div className={styles["info-container-title"]}>Length</div>
+                    </div>
+                    <div className={styles["info-container-value"]}>
+                        {musicTrackLength} min.
+                    </div>
+                </div>
+                <div className={styles["info-container"]}>
+                    <div className={styles["info-container-title-icon-box"]}>
+                        <div className={styles["info-container-icon"]}><PlayArrowIcon /></div>
+                        <div className={styles["info-container-title"]}>Streams</div>
+                    </div>
+                    <div className={styles["info-container-value"]}>
+                        0
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/components/MusicTrackContainer/MusicTrackCard/MusicTrackCard.tsx
+++ frontend/src/components/MusicTrackContainer/MusicTrackCard/MusicTrackCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from "./MusicTrackCard.module.css";
 import Image from 'next/image';
 import { MusicArtistShort } from '@/types/MusicArtist';
+import Link from 'next/link';
 
 type MusicTrackCardType = {
     musicTrack: MusicTrack
@@ -10,25 +11,27 @@ type MusicTrackCardType = {
 
 export default function MusicTrackCard({ musicTrack }: MusicTrackCardType) {
     return (
-        <div className={styles["main-container"]}>
-            <Image
-                quality={50}
-                src={musicTrack.cover_url}
-                alt={musicTrack.title}
-                width={200}
-                height={200}
-                className={styles["image"]}
-                priority
-            />
-            <div className={styles["music-track-info"]}>
-                <div className={styles["music-track-title"]}>{musicTrack.title}</div>
-                <div>
-                    {musicTrack?.music_artists?.map((musicArtist: MusicArtistShort) => {
-                        return (<div key={musicArtist.id}>{musicArtist.name}</div>)
-                    })}
+        <Link href={`/search/track/${musicTrack.id}`}>
+            <div className={styles["main-container"]}>
+                <Image
+                    quality={50}
+                    src={musicTrack.cover_url}
+                    alt={musicTrack.title}
+                    width={200}
+                    height={200}
+                    className={styles["image"]}
+                    priority
+                />
+                <div className={styles["music-track-info"]}>
+                    <div className={styles["music-track-title"]}>{musicTrack.title}</div>
+                    <div>
+                        {musicTrack?.music_artists?.map((musicArtist: MusicArtistShort) => {
+                            return (<div key={musicArtist.id}>{musicArtist.name}</div>)
+                        })}
+                    </div>
+                    <div>{new Date(musicTrack.release_date).getFullYear()}</div>
                 </div>
-                <div>{new Date(musicTrack.release_date).getFullYear()}</div>
             </div>
-        </div>
+        </Link>
     )
 }
