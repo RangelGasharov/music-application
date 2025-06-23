526fe72ccf89bf5d0b14ef6e7852eaa286b29774
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-06-23 23:02:57 +0200

    Added fetch of the music tracks on the playlist single view page

--- frontend/src/app/library/playlists/[playlistId]/page.tsx
+++ frontend/src/app/library/playlists/[playlistId]/page.tsx
@@ -1,3 +1,4 @@
+import { MusicTrack } from '@/types/MusicTrack';
 import { Playlist } from '@/types/Playlist';
 import React from 'react'
 
@@ -19,12 +20,34 @@ async function getPlaylistById(playlistId: string): Promise<Playlist> {
     }
 }
 
+async function getMusicTracksByPlaylistId(playlistId: string): Promise<MusicTrack[]> {
+    try {
+        const res = await fetch(`${process.env.WEB_API_URL}/music-track/playlist/${playlistId}`);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch tracks');
+        }
+
+        const musicTracks = await res.json();
+        return musicTracks;
+    } catch (error) {
+        console.error('Error fetching tracks:', error);
+        throw error;
+    }
+}
+
 export default async function PlaylistSingleViewPage({ params }: { params: Params }) {
     const { playlistId } = await params;
     const playlist = await getPlaylistById(playlistId);
+    const musicTracks = await getMusicTracksByPlaylistId(playlistId);
     return (
         <div>
             <h1>{playlist.title}</h1>
+            <div>
+                {musicTracks.map((musicTrack: MusicTrack) => {
+                    return <div key={musicTrack.id}>{musicTrack.title}</div>
+                })}
+            </div>
         </div>
     )
 }
